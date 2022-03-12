6c71da478c2ce89a5b62492ac72b75654f48cb09
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 14:58:24 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,4 +2,4 @@ echo " Welcome to Employee Wage computation Program on Master Branch"
 check=$((RANDOM%2))
 if(($check==0))
 then echo "Employee is Absent"
-else echo "Employe  is Present"
+else echo "Employe is Present"
