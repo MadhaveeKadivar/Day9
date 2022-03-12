f609df435ae5dc08a32d156ce7c6631adb74b0b3
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 15:02:17 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,14 +5,12 @@ DayHour=0
 if(($check==0))
 then
 	echo "Employee is Absent"
-	echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
 elif(($check==1))
 then
-	echo "Employe is Present for Part Time"
-        DayHour=4
-        echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
-else
 	echo "Employe is Present for Full Time"
-	DayHour=8
-	echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
+        DayHour=8
+else
+	echo "Employe is Present and Woring as Part Time"
+	DayHour=4
 fi
+echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
