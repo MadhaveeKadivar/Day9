498d74c2fdb631ea7a2bbd7888f361a3209cf255
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-12 14:32:38 +0530

    Employee Daily Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,13 @@
-echo " Welcome to Employee Wge computation Program on Master Branch"
+echo " Welcome to Employee Wage computation Program on Master Branch"
 check=$((RANDOM%2))
+wagePerHour=20
+fullDayHour=0
 if(($check==0))
-then echo "Employee is Absent"
-else echo "Employe  is Present"
+then
+	echo "Employee is Absent"
+	echo "Daily Wage of the Employee is : $(($wagePerHour*$fullDayHour))"
+else
+	echo "Employe  is Present"
+	fullDayHour=8
+	echo "Daily Wage of the Employee is : $(($wagePerHour*$fullDayHour))"
+fi
