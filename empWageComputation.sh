ea52aba1ba665b57f4d8fe4dea5cb8dd5e8fc0af
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 15:00:05 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,9 +5,8 @@ DayHour=0
 if(($check==0))
 then
 	echo "Employee is Absent"
-	echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
 else
 	echo "Employe  is Present"
 	DayHour=8
-	echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
 fi
+echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
