ff63124bd0bc3b01db8355ca68986cc11e2cbffc
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 16:20:13 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,17 @@
 echo " Welcome to Employee Wage computation Program on Master Branch"
-check=$((RANDOM%3))
+isPresent=$((RANDOM%3))
 wagePerHour=20
-DayHour=0
-if(($check==0))
+workingHour=0
+if(($isPresent==0))
 then
 	echo "Employee is Absent"
-elif(($check==1))
+	workingHour=0
+elif(($isPresent==1))
 then
 	echo "Employe is Present for Full Time"
-        DayHour=8
+        workingHour=8
 else
 	echo "Employe is Present and Woring as Part Time"
-	DayHour=4
+	workingHour=4
 fi
-echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
+echo "Daily Wage of the Employee is : $(($wagePerHour*$workingHour))"
