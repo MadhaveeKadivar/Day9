30840aaef770e40722a3dcbb929a6d1e39a4d30a
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-12 15:10:19 +0530

    Employee Attendence And Daily Wage Using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,18 @@ echo " Welcome to Employee Wage computation Program on Master Branch"
 check=$((RANDOM%3))
 wagePerHour=20
 DayHour=0
-if(($check==0))
-then
-	echo "Employee is Absent"
-	echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
-elif(($check==1))
-then
-	echo "Employe is Present for Part Time"
-        DayHour=4
-        echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
-else
-	echo "Employe is Present for Full Time"
-	DayHour=8
-	echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
-fi
+case $check in
+	0)
+		echo "Employee is Absent"
+	   	;;
+	1)
+		echo "Employe is Present for Full Time"
+		DayHour=8
+		;;
+	2)
+		echo "Employe is Present and working as Part Time"
+                DayHour=4
+                ;;
+esac
+
+echo "Daily Wage of the Employee is : $(($wagePerHour*$DayHour))"
